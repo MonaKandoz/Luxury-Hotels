6dbf8abde2cf65d0a28dd99ca9468ccf3a0cb2c5
Mona Ramadan Osman <redbaird93@yahoo.com>
2020-06-27 14:42:55 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/bootstrap-grid.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
+        <link href="https://use.fontawesome.com/releases/v5.10.2/css/all.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        
+        <title>Luxury Hotels</title>
+    </head>
+    <body>
+        
+        <!--nav bar start-->
+        <div class="nav">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light">
+
+                    <a class="navbar-brand" href="#">Luxury Hotels</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">features <span class="sr-only">(current)</span></a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="#">menu builder</a>
+                          <a class="dropdown-item" href="#">Custom Link </a>
+                          <a class="dropdown-item" href="#">Integrations</a>
+                        </div>
+                      </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">contact</a>
+                      </li>
+                    </ul>
+                      <ul class="navbar-nav social">
+                          <li class="nav-item">
+                              <i class="fab fa-facebook-f"></i>
+                          </li>
+                          <li class="nav-item">
+                              <i class="fab fa-twitter"></i>
+                          </li>
+                          <li class="nav-item">
+                              <i class="fab fa-instagram"></i>
+                          </li>
+                      </ul>
+
+                  </div>
+
+                </nav>
+           </div>
+        </div>
+        
+        <div class="container">
